4bb9bcf33a2af67f17d7482d20df77a86a4009be
= <=>
2021-08-24 15:44:32 +0700

    Test husky hook

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,12 @@
-
 class NewsController {
-    // [GET] /news
-    index(req, res) {
-        res.render('news');
-    }
-    show(req, res) {
-        res.send('NEW DETAIL!!!');
-    }
-    // [GET] /news/:slug
+  // [GET] /news
+  index(req, res) {
+    res.render('news');
+  }
+  show(req, res) {
+    res.send('NEW DETAIL!!!');
+  }
+  // [GET] /news/:slug
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan'); 
+const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const exp = require('constants');
 
@@ -15,15 +15,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars({
-  extname:'.hbs'
-}));
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views')); 
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
